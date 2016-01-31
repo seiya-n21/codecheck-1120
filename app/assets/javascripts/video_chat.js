bcd54d72a9bd4ece78dc5170399ffaefc32d4cfe
brown0702 <brown070288@gmail.com>
2016-01-31 17:27:08 +0900

    testsuccess-to-vote

--- app/assets/javascripts/video_chat.js
+++ app/assets/javascripts/video_chat.js
@@ -50,7 +50,7 @@ function manage_mediasteam(s_roomid, s_name, s_gender, uid) {
 
   // Error handling:★ユーザに何を表示すべきか要検討
   multiparty.on('error', function(err) {
-	  alert(err);
+	  //alert(err);
 	});
 }
 
--- spec/features/castings_spec.rb
+++ spec/features/castings_spec.rb
@@ -11,42 +11,115 @@ RSpec.feature "Castings", type: :feature do
    end
    Capybara.current_driver = :selenium
 
-   scenario "start casting", js: true do
-     visit root_path
-     expect(current_path).to eq root_path
-
-     expect{
-       fill_in 'user[name]', with: 'user01'
-       find('#female').click
-       click_button 'CAST NOW'
-     }.to change(User, :count).by(1).and change(Room, :count).by(1)
-  
-     expect(current_path).to eq room_path
-     expect(page).to have_content '4人集まるまで待ってね'
-     sleep 2
-     # save_and_open_page
-   end
-  
+#   scenario "start casting", js: true do
+#     visit root_path
+#     expect(current_path).to eq root_path
+#
+#     expect{
+#       fill_in 'user[name]', with: 'user01'
+#       find('#female').click
+#       click_button 'CAST NOW'
+#     }.to change(User, :count).by(1).and change(Room, :count).by(1)
+#  
+#     expect(current_path).to eq room_path
+#     expect(page).to have_content '4人集まるまで待ってね'
+#     sleep 2
+#   end
+#  
    scenario "start video chat", js: true do
-     room = Room.create
-     room.users.create(name: 'user01', gender: 'male')
-     room.users.create(name: 'user02', gender: 'female')
-     room.users.create(name: 'user03', gender: 'female')
-  
-     visit root_path
-     expect(current_path).to eq root_path
 
-     expect{
-       fill_in 'user[name]', with: 'user04'
-       find('#male').click
-       click_button 'CAST NOW'
-     }.to change(User, :count).by(1).and change(Room, :count).by(0)
+     using_session :user1 do
+       visit root_path
+       expect(current_path).to eq root_path
   
-     user = User.find_by(name: 'user04')
-     expect(user.room.status).to be_truthy
+       expect{
+         fill_in 'user[name]', with: 'user01'
+         find('#female').click
+         click_button 'CAST NOW'
+       }.to change(User, :count).by(1).and change(Room, :count).by(1)
   
-     expect(current_path).to eq room_path
-     expect(page).to_not have_content '4人集まるまで待ってね'
+       sleep 2
+       find('#camera-icon').click
+       find('#voice-icon').click
+     end
+
+     using_session :user2 do
+       visit root_path
+       expect(current_path).to eq root_path
+
+       expect{
+         fill_in 'user[name]', with: 'user02'
+         find('#female').click
+         click_button 'CAST NOW'
+       }.to change(User, :count).by(1).and change(Room, :count).by(0)
+     
+       sleep 2
+       find('#camera-icon').click
+       find('#voice-icon').click
+     end
+     using_session :user3 do
+       visit root_path
+       expect(current_path).to eq root_path
+
+       expect{
+         fill_in 'user[name]', with: 'user03'
+         find('#male').click
+         click_button 'CAST NOW'
+       }.to change(User, :count).by(1).and change(Room, :count).by(0)
+     
+       sleep 2
+       find('#camera-icon').click
+       find('#voice-icon').click
+     end
+     using_session :user4 do
+       visit root_path
+       expect(current_path).to eq root_path
+
+       expect{
+         fill_in 'user[name]', with: 'user04'
+         find('#male').click
+         click_button 'CAST NOW'
+       }.to change(User, :count).by(1).and change(Room, :count).by(0)
+     
+       sleep 2
+       find('#camera-icon').click
+       find('#voice-icon').click
+
+       user = User.find_by(name: 'user04')
+       expect(user.room.status).to be_truthy
+    
+       expect(current_path).to eq room_path
+       expect(page).to_not have_content '4人集まるまで待ってね'
+     end
+
+     sleep 15
+
+     using_session :user4 do
+       expect(current_path).to eq vote_path
+       choose 'candidate_1'
+       click_button 'submit'
+     end
+     using_session :user3 do
+       expect(current_path).to eq vote_path
+       choose 'candidate_1'
+       click_button 'submit'
+     end
+     using_session :user2 do
+       expect(current_path).to eq vote_path
+       choose 'candidate_4'
+       click_button 'submit'
+     end
+     using_session :user1 do
+       expect(current_path).to eq vote_path
+       choose 'candidate_4'
+       click_button 'submit'
+     end
+
+     using_session :user1 do
+       fill_in "line", :with "hello!"
+       click_on "send"
+     end 
+
    end
 
 end
