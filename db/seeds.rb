bcde900f5e944a559abcc44bae972ef6997bd3cc
mats0228 <m.hisa3153@gmail.com>
2016-02-05 15:35:02 +0900

    /bin/rubocop に成功するようにフォーマット修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,5 @@
 # encoding: UTF-8
-
-
-## 男:1/女:1 入室したらカウンタ表示される初期条件
-
+# 男:1/女:1 入室したらカウンタ表示される初期条件
 room = Room.create
 room.users.create(name: 'user01', gender: 'male')
 room.users.create(name: 'user02', gender: 'female')
