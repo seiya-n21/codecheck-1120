7d249d88bbf51762f77f8969c02415f05d1443f7
mats0228 <m.hisa3153@gmail.com>
2016-02-05 13:01:37 +0900

    試験しやすくするための修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
-room = Room.create
+# encoding: UTF-8
 
+
+## 男:1/女:1 入室したらカウンタ表示される初期条件
+
+room = Room.create
 room.users.create(name: 'user01', gender: 'male')
 room.users.create(name: 'user02', gender: 'female')
-room.users.create(name: 'user03', gender: 'female')
+room.create(male: 1, female: 1)
