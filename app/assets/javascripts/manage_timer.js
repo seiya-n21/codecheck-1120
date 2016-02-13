77f955a062b33f22a9e448824c52989c4c2c0ff3
mats0228 <m.hisa3153@gmail.com>
2016-02-13 12:49:42 +0900

    bagfix

--- app/assets/javascripts/manage_timer.js
+++ app/assets/javascripts/manage_timer.js
@@ -9,7 +9,7 @@ var fix_time;
 
 function display_timer(){ //再帰呼び出し
   var now_time = (new Date).getTime();
-  var left_time = (target_time - now_time + fix_time)/1000; // 1,000 ms -> 1 s
+  var left_time = (target_time - now_time - fix_time)/1000; // 1,000 ms -> 1 s
 
   if(left_time < 0){ // 終了判定・処理
     logging_debug('ridirect to: ' + VOTE_URL);
@@ -19,7 +19,7 @@ function display_timer(){ //再帰呼び出し
   } else {
     // m:残時間を60でわった値  s:残時間を60で割った余り
     var m = Math.floor(left_time / 60);
-    var s = left_time % 60;
+    var s = Math.floor(left_time % 60);
     s = (s < 0) ? 0 : s; //マイナスならないよう修正
     var s_timer = ( (m > 0) ? (m + '分' + s + '秒') : (s + '秒') );
     $("#room_timer").html('<p id="timer_counter" > 終了まで' + s_timer + '</p>');
@@ -29,15 +29,14 @@ function display_timer(){ //再帰呼び出し
 // target_time: Serverから返却された時間を基にTarget決める
 // fix_time:    ローカル時刻とサーバ時刻から差分を補正する
 function set_timer_variable(server_target_time){
-  //  var server_target_time = '05 Feb 2016 02:13:32 UTC +00:00' //debug
-  d = new Date(server_target_time);
-  var d2 = new Date();
-  var client_time = d2.getTime();
+  st = new Date(server_target_time);
+  var ct = new Date();
+  var client_time = ct.getTime();
 
-  fix_time = d.getTime() - client_time;
+  fix_time = st.getTime() - client_time;
   // TODO: 600は、gon.const.video_time　に置き換え
-  target_time  = d.setSeconds(d.getSeconds() + 600); 
-
+  d = new Date();
+  target_time  = d.setTime(st.getTime() + (10 * 60 * 1000) ); 
   logging_debug('server_time:  ');
   logging_debug(d.getTime());
   logging_debug('client_time:  ');
